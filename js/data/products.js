61b6770c9f635d193102c314b0e36aae0ce799a4
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-11 00:58:28 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,345 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Steel Turner',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Sturdy brushed stainless steel construction',
+            },
+            {
+                text: 'Subtle, soft, non-slip grip',
+            },
+            {
+                text: 'Stainless steel surface for safely leaning utensils on hot cookware',
+            },
+            {
+                text: 'Dishwasher safe',
+            },
+            {
+                text: 'Hole in handle for hanging',
+            }
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Digital Kitchen Scale',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Stainless steel digital kitchen scale with widescreen LCD screen',
+            },
+            {
+                text: 'Weighs up to 11 pounds (about 5000 grams) of food (minimum of 2 grams)',
+            },
+            {
+                text: 'Advanced sensor technology delivers quick, accurate responses',
+            },
+            {
+                text: 'Displays results in ounces, grams, pounds',
+            },
+            {
+                text: 'Tare button subtracts container weight to zero out the scale so you only measure what you want',
+            },
+            {
+                text: 'Measures approximately 8.7x7.1x1.6 inches (LxWxH)',
+            },
+            {
+                text: 'BPA-free',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Bicycle helmet',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Dial fit offers 100% adjustability for larger or smaller heads',
+            },
+            {
+                text: '13 vents provide efficient air circulation',
+            },
+            {
+                text: 'Full shell coverage for ultimate protection',
+            },
+            {
+                text: 'Removable visor protects you from the elements',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Baby carrier',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Design',
+                text: '4-in-1 convertible carrier with adjustable ergonomic seat with adjustable shoulder straps and waist belt to shift weight to the wearers hips for a more comfortable fit',
+            },
+            {
+                title: 'Wearing positions',
+                text: 'Convertible facing-in and facing-out design for newborns and older babies, weighing between 8-32 pounds',
+            },
+            {
+                title: 'Benefits and use',
+                text: 'Experience hands-free convenience with opportunities for bonding and developmental benefits, whether it’s running errands or exploring parks, there’s no place like close',
+            },
+            {
+                title: 'Adjustability',
+                text: 'Adjustable 2-position seat, padded shoulder straps and supportive waist belt, designed to fit a wide range of body types and lifestyles',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png' , './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Multipurpose Rolling Cart',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Versatile',
+                text: 'Ideal for extra work/storage space next to a desk, as a printing/supply station in an office, or for small appliances in a kitchen',
+            },
+            {
+                title: 'Adjustable shelf',
+                text: 'The shelf can be raised or lowered to accommodate differently sized items',
+            },
+            {
+                title: 'Mobile',
+                text: 'Smooth-rolling wheels allow for effortlessly moving the cart from one location to another',
+            },
+            {
+                title: 'Durable construction',
+                text: 'Made of 60% metal and 40% particle board for sturdy, reliable strength',
+            },
+            {
+                title: 'Dims and warranty',
+                text: 'Measures 20.5 by 20.5 by 30 inches (simple assembly required)',
+            }
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Belgian Waffle Maker',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Make waffles quickly and easily',
+                text: 'Make fluffy Belgian waffles in just a few minutes',
+            },
+            {
+                title: 'Adjustable browning control',
+                text: 'Adjustable browning control makes the waffle crispy on the outside, fluffy on the inside',
+            },
+            {
+                title: 'Easy to clean',
+                text: 'Nonstick waffle plates wipe down easily',
+            },
+            {
+                title: 'Power and ready lights',
+                text: 'Power on and ready lights indicate when the waffle maker is turned on and when to add batter',
+            },
+            {
+                title: 'Compact, upright storage',
+                text: 'Takes up minimal space in cabinets',
+            }
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Enameled Cast Iron Oven',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                title: 'Sloped sides deliver perfect performance',
+                text: 'The sloped sides of the skillet maximize cooking surface. Skillets are designed for browning and tossing small pieces of food like mushrooms.',
+            },
+            {
+                title: 'Hard anodized exterior',
+                text: 'Cookware exterior is harder than stainless steel for durability and professional performance. Dense, non-porous, highly wear resistant.',
+            },
+            {
+                title: 'Unsurpassed heat distribution',
+                text: 'Hard anodized construction heats quickly and spreads heat evenly. Eliminates hot spots.',
+            },
+            {
+                title: 'Quantanium non-stick for professional results',
+                text: 'Quantanium nonstick cooking surface is reinforced with titanium for lasting food release, healthy cooking and easy cleanup. Metal utensil safe. Great for slowly cooking minced garlic in olive oil, frying breaded pork chops, or browning sausage.',
+            },
+            {
+                title: 'Cool grip handle',
+                text: 'Solid stainless steel riveted handle stays cool on the stovetop.',
+            },
+            {
+                title: 'Drip-free pouring',
+                text: 'Rim is tapered for drip-free pouring.',
+            },
+            {
+                title: 'Constructed to last',
+                text: 'Lifetime warranty.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.jpg'],
+    },
+    {
+        id: 'product-8',
+        name: 'Vase',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'This simple and elegant glass vase will add depth to any living space and setting',
+            },
+            {
+                text: 'Make a statement with this exquisite glass vase handcrafted by skilled artisans',
+            },
+            {
+                text: 'Soft lines and multi-layered swirl patterns are unique to each vase and adds depth and layers to any setting',
+            },
+            {
+                text: 'Smoked grey color near top of vase designed to let a single flower or floral arrangement stand out',
+            },
+            {
+                text: 'Measures 8.75" W x 5." L x 14.25" H. Swirls are unique to each vase. Vase opening measures approximately 2.1 inches.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Binoculars for Adults',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Multi-coated optics and bak-4 prisms',
+                text: 'Multi-coated optics help obtain high resolution and high contrast views, while the prisms made of BaK-4 glass will give you enhanced color fidelity.',
+            },
+            {
+                title: 'Waterproof and fogproof',
+                text: 'Designed to withstand all weather conditions, our  compact binoculars have been filled and sealed with dry nitrogen gas for rigorous outdoor use without internal fogging of the lenses.',
+            },
+            {
+                title: 'Protective rubber covering',
+                text: 'Our binocular is both durable and well-armored; its rugged exterior ensures protection from all outdoor elements while providing a secure, anti-slip gripping surface.',
+            },
+            {
+                title: 'Twist-up eyecups for quick adjustment',
+                text: 'Large focus knobs make for easy and precise focusing, and twist-up eyecups allow for quick adjustment of eye relief to obtain a full field of view. Eyecups can be easily positioned for use with or without eyeglasses.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Wireless Controller',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                title: 'Haptic feedback',
+                text: 'Feel physically responsive feedback to your in-game actions with dual actuators which replace traditional rumble motors. In your hands, these dynamic vibrations can simulate the feeling of everything from environments to the recoil of different weapons.',
+            },
+            {
+                title: 'Adaptive triggers',
+                text: 'Experience varying levels of force and tension as you interact with your in-game gear and environments. From pulling back an increasingly tight bowstring to hitting the brakes on a speeding car, feel physically connected to your on-screen actions.',
+            },
+            {
+                title: 'Built-in microphone and headset jack',
+                text: 'Chat with friends online using the built-in microphone or by connecting a headset to the 3.5mm jack. Easily switch voice capture on and off at a moment’s notice with the dedicated mute button.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Adjustable Inline Skates',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Adjustable Size',
+                text: 'Adjustable roller skates has: size L(foot-length:216-241mm), size XL (foot-length: 252-270mm), adjust the length of boot by the push button before wearing, Please make sure to choose the correct size by checking enclosed size chart pictures.',
+            },
+            {
+                title: 'Features Upgrade',
+                text: 'The buckle & Velcro strap & lace closure system secure the feet inside the skates to enhance the fit and add additional support. Durable frame a vibration-absorbing composite frame that is interlocked with the base, keeping users more balanced by being lower to the ground.',
+            },
+            {
+                title: 'Safety & Smooth',
+                text: 'Reinforced aluminium frame, 76mm 82A wheels and ABEC 7 bearings are a good combination for rolling at a moderate speed with a little less effort due to the slightly faster bearing. enhance stability, balance and control.',
+            },
+            {
+                title: 'Comfortable',
+                text: 'Designed with Polyester Fabric Mesh, Makes the inline skate breathable and comfortable to prevent sweat smell. Great padding in the Training liner ensures comfort for any skater. The closure system adds to the overall fit.',
+            },
+        ],
+        images: ['./img/img11-1.jpg', './img/img11-2.jpg', './img/img11-3.jpg'],
+    },
+    {
+        id: 'product-12',
+        name: 'Wireless Headphones',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Deep, immersive sound, improved EQ best in class performance for wireless headphones.',
+            },
+            {
+                text: 'Latest Bluetooth technology for easy connectivity and seamless audiovideo syncs.',
+            },
+            {
+                text: 'Advanced microphone system, HD voice for clear calls in windy or noisy environments.',
+            },
+            {
+                text: 'Up to 15 hours play time with rechargeable lithium ion battery.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png'],
+    },
+    {
+        id: 'product-13',
+        name: 'Video Baby Monitor',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Parent unit features large 5” LCD color screen with LED sound level indicator and rechargeable battery with low battery alert.',
+            },
+            {
+                text: 'Room temperature display, two-way communication, 5 soothing lullabies and split-screen viewing (requires additional camera MBP36XLBU, sold separately).',
+            },
+            {
+                text: 'Portable camera features rechargeable battery with remote pan, tilt and zoom, infrared night vision, high sensitivity microphone, and wireless technology - 2.4 GHz (for Local Viewing).',
+            },
+            {
+                text: 'Up to 1000ft range with out-of-range alert, expandable up to 4 Cameras.',
+            },
+        ],
+        images: ['./img/img13-1.png'],
+    },
+]
